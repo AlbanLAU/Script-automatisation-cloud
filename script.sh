2fe6aa1b3b69cabe6e66b371f2538c18c56d7eb0
Mathieuepf <91480893+Mathieuepf@users.noreply.github.com>
2024-12-18 11:03:32 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
-sudo dpkg --configure -a 
 sudo apt-get update && \
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common && \
+sudo dpkg --configure -a  \
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && \
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" && \
 sudo apt-get update && \
