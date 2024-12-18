32ebec3db3ba87010b02e1a69b01e65afd333a21
Mathieuepf <91480893+Mathieuepf@users.noreply.github.com>
2024-12-18 11:14:47 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
-sudo dpkg --configure -a  \
-sudo apt-get update && \
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common && \
+sudo dpkg --configure -a 
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common 
